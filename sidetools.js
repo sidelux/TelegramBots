ea57716c7612b61cf8778276b6f90fe45ab64dfa
root <redfenix45@gmail.com>
2023-02-23 14:30:12 +0100

    [SideTools] Added second group for partecipation

--- sidetools.js
+++ sidetools.js
@@ -70,52 +70,90 @@ process.on('SIGTERM', function() {
 });
 
 bot.onText(/^\/start/i, function (message) {
-	if (message.chat.id < 0)
-		return;
-
 	bot.sendMessage(message.chat.id, "Bot realizzato per fornire particolari funzionalità nei gruppi, creato da @fenix45.");
 	if (message.text != undefined)
 		console.log(getNow("it") + " " + message.from.username + " - " + message.text);
 });
 
-var originalText = "Ci sei per giocare oggi? 👀";
+var originalText1 = "Ci sei per giocare oggi? 👀";
+var originalText2 = "Ci sarai all'allenamento di stasera? 🔥";
 
 bot.onText(/^\/presenze/i, function (message) {
-	if (message.chat.id != "-461536160")
+	console.log(getNow("it") + " " + message.from.username + " - " + message.text);
+	let now = new Date();
+	let now_day = now.getFullYear() + "-" + addZero(now.getMonth()+1) + "-" + now.getDate();
+	var poll_cnt = connection_sync.query("SELECT COUNT(id) As cnt FROM partecipation WHERE add_date LIKE '" + now_day + "%' AND chat_id = '" + message.chat.id + "'")[0].cnt;
+	if (poll_cnt > 0) {
+		bot.sendMessage(message.chat.id, "Oggi è stato già pubblicato un sondaggio!");
 		return;
-
+	}
 	var partecipation_id = connection_sync.query("SELECT MAX(id)+1 As new_id FROM partecipation")[0].new_id;
 	if (partecipation_id == null)
 		partecipation_id = 1;
-	var iKeys = [];
-	iKeys.push([{
-		text: "Sì",
-		callback_data: partecipation_id + ":partecipation:yes"
-	},{
-		text: "No",
-		callback_data: partecipation_id + ":partecipation:no"
-	},{
-		text: "Forse",
-		callback_data: partecipation_id + ":partecipation:maybe"
-	}]);
-	bot.deleteMessage(message.chat.id, message.message_id);
-	bot.sendMessage(message.chat.id, originalText, {
-		parse_mode: 'HTML',
-		reply_markup: {
-			inline_keyboard: iKeys
-		}
-	}).then(function (msg) {
-		connection.query("INSERT INTO partecipation (chat_id, message_id) VALUES (" + msg.chat.id + ", " + msg.message_id + ")", function (err, rows) {
-			if (err) throw err;
+	if (message.chat.id == "-461536160") {
+		var iKeys = [];
+		iKeys.push([{
+			text: "Sì",
+			callback_data: partecipation_id + ":partecipation:yes"
+		},{
+			text: "No",
+			callback_data: partecipation_id + ":partecipation:no"
+		},{
+			text: "Forse",
+			callback_data: partecipation_id + ":partecipation:maybe"
+		}],[{
+			text: "20:30",
+			callback_data: partecipation_id + ":partecipation:2030"
+		},{
+			text: "21:00",
+			callback_data: partecipation_id + ":partecipation:2100"
+		},{
+			text: "21:30",
+			callback_data: partecipation_id + ":partecipation:2130"
+		},{
+			text: "22:00",
+			callback_data: partecipation_id + ":partecipation:2200"
+		}]);
+		bot.deleteMessage(message.chat.id, message.message_id);
+		bot.sendMessage(message.chat.id, originalText1, {
+			parse_mode: 'HTML',
+			reply_markup: {
+				inline_keyboard: iKeys
+			}
+		}).then(function (msg) {
+			connection.query("INSERT INTO partecipation (chat_id, message_id) VALUES (" + msg.chat.id + ", " + msg.message_id + ")", function (err, rows) {
+				if (err) throw err;
+			});
+			bot.unpinAllChatMessages(msg.chat.id).then(function (data) {
+				bot.pinChatMessage(msg.chat.id, msg.message_id, {disable_notification: true});
+			});
 		});
-		bot.unpinAllChatMessages(msg.chat.id).then(function (data) {
+	} else if (message.chat.id == "-1001865921442") {
+		var iKeys = [];
+		iKeys.push([{
+			text: "Sì",
+			callback_data: partecipation_id + ":partecipation:yes"
+		},{
+			text: "No",
+			callback_data: partecipation_id + ":partecipation:no"
+		}]);
+		bot.deleteMessage(message.chat.id, message.message_id);
+		bot.sendMessage(message.chat.id, originalText2, {
+			parse_mode: 'HTML',
+			reply_markup: {
+				inline_keyboard: iKeys
+			}
+		}).then(function (msg) {
+			connection.query("INSERT INTO partecipation (chat_id, message_id) VALUES (" + msg.chat.id + ", " + msg.message_id + ")", function (err, rows) {
+				if (err) throw err;
+			});
 			bot.pinChatMessage(msg.chat.id, msg.message_id, {disable_notification: true});
 		});
-	});
+	}
 });
 
 bot.onText(/^\/partecipanti (.+)/i, function (message, match) {
-	if (message.chat.id != "-461536160")
+	if ((message.chat.id != "-461536160") || (message.message_id != "-1001865921442"))
 		return;
 
 	bot.getChatMember(message.chat.id, message.from.id).then(function (data) {
@@ -163,27 +201,58 @@ bot.on('callback_query', function (message) {
 		username = "@" + message.from.username;
 	let user_id = message.from.id;
 	if (fun == "partecipation") {
-		connection.query("SELECT id, user_id, response FROM partecipation_user WHERE partecipation_id = " + id + " AND user_id = " + user_id, function (err, rows) {
+		connection.query("SELECT add_date FROM partecipation WHERE id = " + id, function (err, rows) {
 			if (err) throw err;
-			if (Object.keys(rows).length == 0) {
-				connection.query("INSERT INTO partecipation_user (partecipation_id, user_id, username, response) VALUES (" + id + ", " + user_id + ", '" + username + "', '" + param + "')", function (err, rows) {
-					if (err) throw err;
-					bot.answerCallbackQuery(message.id, {text: "Ok!"});
-					printPartecipations(message, id);
-				});
-			} else {
-				let partecipation_user_id = rows[0].id;
-				let response = rows[0].response;
-				if (response == param) {
-					bot.answerCallbackQuery(message.id, {text: "Hai già votato questa opzione!"});
-				} else {
-					connection.query("UPDATE partecipation_user SET response = '" + param + "' WHERE id = " + partecipation_user_id, function (err, rows) {
-						if (err) throw err;
-						printPartecipations(message, id);
-					});
-					bot.answerCallbackQuery(message.id, {text: "Ok!"});
-				}
+			let add_date = new Date(Date.parse(rows[0].add_date));
+			let now = new Date();
+			if (add_date.getFullYear() + "-" + (add_date.getMonth()+1) + "-" + add_date.getDate() != now.getFullYear() + "-" + (now.getMonth()+1) + "-" + now.getDate()) {
+				bot.answerCallbackQuery(message.id, {text: "Sondaggio scaduto!"});
+				return;
 			}
+
+			connection.query("SELECT id, user_id, response, time FROM partecipation_user WHERE partecipation_id = " + id + " AND user_id = " + user_id, function (err, rows) {
+				if (err) throw err;
+				if (Object.keys(rows).length == 0) {
+					if (isNaN(param)) {
+						connection.query("INSERT INTO partecipation_user (partecipation_id, user_id, username, response) VALUES (" + id + ", " + user_id + ", '" + username + "', '" + param + "')", function (err, rows) {
+							if (err) throw err;
+							printPartecipations(message, id);
+						});
+					} else {
+						connection.query("INSERT INTO partecipation_user (partecipation_id, user_id, username, response, time) VALUES (" + id + ", " + user_id + ", '" + username + "', 'yes', '" + param + "')", function (err, rows) {
+							if (err) throw err;
+							printPartecipations(message, id);
+						});
+					}
+					bot.answerCallbackQuery(message.id, {text: "Ok!"});
+				} else {
+					let partecipation_user_id = rows[0].id;
+					let response = rows[0].response;
+					let time = rows[0].time;
+					if (response == param) {
+						bot.answerCallbackQuery(message.id, {text: "Hai già votato questa opzione!"});
+					} else if (time == param) {
+						connection.query("UPDATE partecipation_user SET time = NULL WHERE id = " + partecipation_user_id, function (err, rows) {
+							if (err) throw err;
+							printPartecipations(message, id);
+						});
+						bot.answerCallbackQuery(message.id, {text: "Orario rimosso!"});
+					} else {
+						if (isNaN(param)) {
+							connection.query("UPDATE partecipation_user SET response = '" + param + "' WHERE id = " + partecipation_user_id, function (err, rows) {
+								if (err) throw err;
+								printPartecipations(message, id);
+							});
+						} else {
+							connection.query("UPDATE partecipation_user SET time = '" + param + "' WHERE id = " + partecipation_user_id, function (err, rows) {
+								if (err) throw err;
+								printPartecipations(message, id);
+							});
+						}
+						bot.answerCallbackQuery(message.id, {text: "Ok!"});
+					}
+				}
+			});
 		});
 	}
 });
@@ -193,7 +262,7 @@ bot.onText(/^\/openai (.+)/i, async function (message, match) {
 		return;
 
 	const configuration = new Configuration({
-		apiKey: "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",
+		apiKey: "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",
 	});
 	const openai = new OpenAIApi(configuration);
 
@@ -235,55 +304,118 @@ function printPartecipations(message, id) {
 		var total_partecipants = -1;
 		if (Object.keys(rows).length > 0)
 			total_partecipants = rows[0].partecipants;
-		connection.query("SELECT username, response FROM partecipation_user WHERE partecipation_id = " + id, function (err, rows) {
+		connection.query("SELECT username, response, time FROM partecipation_user WHERE partecipation_id = " + id, function (err, rows) {
 			if (err) throw err;
 			var voted_partecipations = Object.keys(rows).length;
-			var newText = originalText + "\n\n";
 			
-			var c = 0;
-			var partText = "";
-			for (var i = 0; i < Object.keys(rows).length; i++) {
-				if (rows[i].response == "yes") {
-					partText += rows[i].username + "\n";
-					c++;
-				}
-			}
-			if (c > 0)
-				newText += "Sì:\n" + partText + "\n";
+			if (message.message.chat.id == "-461536160") {
+				var newText = originalText1 + "\n\n";
 
-			var c = 0;
-			var partText = "";
-			for (var i = 0; i < Object.keys(rows).length; i++) {
-				if (rows[i].response == "maybe") {
-					partText += rows[i].username + "\n";
-					c++;
+				var c = 0;
+				var partText = "";
+				for (var i = 0; i < Object.keys(rows).length; i++) {
+					if (rows[i].response == "yes") {
+						var time = "";
+						if (rows[i].time != null)
+							time = " (" + rows[i].time.slice(0,2) + ":" + rows[i].time.slice(2) + ")";
+						partText += rows[i].username + time + "\n";
+						c++;
+					}
 				}
-			}
-			if (c > 0)
-				newText += "Forse:\n" + partText + "\n";
+				if (c > 0)
+					newText += "Sì:\n" + partText + "\n";
 
-			var c = 0;
-			var partText = "";
-			for (var i = 0; i < Object.keys(rows).length; i++) {
-				if (rows[i].response == "no") {
-					partText += rows[i].username + "\n";
-					c++;
+				var c = 0;
+				var partText = "";
+				for (var i = 0; i < Object.keys(rows).length; i++) {
+					if (rows[i].response == "maybe") {
+						var time = "";
+						if (rows[i].time != null)
+							time = " (" + rows[i].time.slice(0,2) + ":" + rows[i].time.slice(2) + ")";
+						partText += rows[i].username + time + "\n";
+						c++;
+					}
 				}
-			}
-			if (c > 0)
-				newText += "No:\n" + partText + "\n";
+				if (c > 0)
+					newText += "Forse:\n" + partText + "\n";
 
-			var iKeys = [];
-			iKeys.push([{
-				text: "Sì",
-				callback_data: id + ":partecipation:yes"
-			},{
-				text: "No",
-				callback_data: id + ":partecipation:no"
-			},{
-				text: "Forse",
-				callback_data: id + ":partecipation:maybe"
-			}]);
+				var c = 0;
+				var partText = "";
+				for (var i = 0; i < Object.keys(rows).length; i++) {
+					if (rows[i].response == "no") {
+						var time = "";
+						if (rows[i].time != null)
+							time = " (" + rows[i].time.slice(0,2) + ":" + rows[i].time.slice(2) + ")";
+						partText += rows[i].username + time + "\n";
+						c++;
+					}
+				}
+				if (c > 0)
+					newText += "No:\n" + partText + "\n";
+
+				var iKeys = [];
+				iKeys.push([{
+					text: "Sì",
+					callback_data: id + ":partecipation:yes"
+				},{
+					text: "No",
+					callback_data: id + ":partecipation:no"
+				},{
+					text: "Forse",
+					callback_data: id + ":partecipation:maybe"
+				}],[{
+					text: "20:30",
+					callback_data: id + ":partecipation:2030"
+				},{
+					text: "21:00",
+					callback_data: id + ":partecipation:2100"
+				},{
+					text: "21:30",
+					callback_data: id + ":partecipation:2130"
+				},{
+					text: "22:00",
+					callback_data: id + ":partecipation:2200"
+				}]);
+			} else if (message.message.chat.id == "-1001865921442") {
+				var newText = originalText2 + "\n\n";
+
+				var c = 0;
+				var partText = "";
+				for (var i = 0; i < Object.keys(rows).length; i++) {
+					if (rows[i].response == "yes") {
+						var time = "";
+						if (rows[i].time != null)
+							time = " (" + rows[i].time.slice(0,2) + ":" + rows[i].time.slice(2) + ")";
+						partText += rows[i].username + time + "\n";
+						c++;
+					}
+				}
+				if (c > 0)
+					newText += "Sì:\n" + partText + "\n";
+
+				var c = 0;
+				var partText = "";
+				for (var i = 0; i < Object.keys(rows).length; i++) {
+					if (rows[i].response == "no") {
+						var time = "";
+						if (rows[i].time != null)
+							time = " (" + rows[i].time.slice(0,2) + ":" + rows[i].time.slice(2) + ")";
+						partText += rows[i].username + time + "\n";
+						c++;
+					}
+				}
+				if (c > 0)
+					newText += "No:\n" + partText + "\n";
+
+				var iKeys = [];
+				iKeys.push([{
+					text: "Sì",
+					callback_data: id + ":partecipation:yes"
+				},{
+					text: "No",
+					callback_data: id + ":partecipation:no"
+				}]);
+			}
 
 			if (total_partecipants != -1)
 				newText += "Votanti: " + voted_partecipations + "/" + total_partecipants;
